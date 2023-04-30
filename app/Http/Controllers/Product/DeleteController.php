0a40a3976928a2c54d3df9397f2c4d2b26f21468
RABID-DUCK <54327392+RABID-DUCK@users.noreply.github.com>
2023-04-30 13:19:02 +0500

    add filter

--- app/Http/Controllers/Product/DeleteController.php
+++ app/Http/Controllers/Product/DeleteController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Product\StoreRequest;
+use App\Http\Requests\Product\IndexRequest;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,16 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use Filterable;
+
    protected $table = 'products';
    protected $guarded = false;
 
    public function category(){
        return $this->belongsTo(Category::class, 'category_id', 'id');
+   }
+
+   public function tags(){
+       return $this->belongsToMany(Tag::class, 'product_tags', 'product_id', 'tag_id');
    }
 
    public function getImageUrlAttribute(){
